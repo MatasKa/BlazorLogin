476a774ee1f49b9f0fabd71290ef3e995a711a40
Matas K <matas.k2002@gmail.com>
2022-11-08 17:28:51 +0100

    Initial commit

---
+++ BlazorLogin/Program.cs
@@ -0,0 +1,18 @@
+// Blazor WASM (client-size) Program.cs file
+
+//using LoginForm.Client;
+using BlazorLogin;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddSingleton<ILocalStorage, LocalStorage>(); // NOTE: this line is newly added
+
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
